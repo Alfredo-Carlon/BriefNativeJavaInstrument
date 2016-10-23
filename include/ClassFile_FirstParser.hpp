9178b647f5bbdfd3379727b1b6fdd7f0b7408222
J. Alfredo Cruz Carlon <alfredo.carlon@gmail.com>
2016-10-23 13:37:43 -0500

    Add files via upload

---
+++ include/ClassFile_FirstParser.hpp
@@ -0,0 +1,317 @@
+//
+//  ClassFile_FirstParser.hpp
+//  
+//
+//  Created by Alfredo Cruz on 9/30/16.
+//
+//
+
+#ifndef ClassFile_FirstParser_hpp
+#define ClassFile_FirstParser_hpp
+
+#include <ClassFile_def.hpp>
+
+/**
+ * First 'quick intended' interface for reading and parsing
+ * portions of a .class file
+ **/
+
+class ClassFile_FP {
+
+private:
+    ClassFile *_classFile;
+    
+    //Struct to hold defined names already in the Constant Pool.
+    struct _dCPa {
+        unsigned short ConstantValue;
+        
+        _dCPa(){
+            ConstantValue = -1;
+        }
+    }_definedCPAttributes;
+    
+    
+    //Counter for the next constant pool entry
+    unsigned int _constant_pool_next_index;
+    
+    //Holds the size in bytes of the current bytecode
+    unsigned long _size_in_bytes;
+    
+    //Fill the field_info & method_info structures array
+    //structures.
+    //fi        is a pointer to a field_info array
+    //fi_count  is the number of fields/methods to fill
+    //bytecode  is a pointer to the bytecode
+    //returns   a pointer to the next byte in bytecode
+    unsigned char * _fillFieldAttribute(field_info **,
+                                              unsigned short,
+                                              unsigned char *);
+    
+    
+    //Fill the method_info structures array
+    //structures.
+    //fi        is a pointer to a field_info array
+    //fi_count  is the number of fields/methods to fill
+    //bytecode  is a pointer to the bytecode
+    //returns   a pointer to the next byte in bytecode
+    unsigned char * _fillMethodAttribute(method_info **,
+                                              unsigned short,
+                                              unsigned char *);
+    
+    //Pushes a value into _classFile's constant pool
+    void _push_into_constant_pool(cp_info& data);
+    
+    
+    
+public:
+    
+    //Default constructor
+    ClassFile_FP();
+    //Destructor
+    ~ClassFile_FP();
+    
+    
+    //Load the bytecode into _classFile
+    //payload       is a pointer to the bytecode
+    //payloadSize   is the size in bytes of the bytecode
+    void loadBytecode(unsigned char *payload, unsigned int payloadSize);
+    
+    //Dumps the bytecode to disk at 'filename'
+    void dumpBytecode(const char *filename);
+    
+    
+    
+    
+    //Returns the number of elements in the
+    //constant pool of _classFile
+    unsigned int constantPoolSize();
+    
+    //Returns the number of fields
+    //Declared in _classFile
+    unsigned int fieldsSize();
+    
+    //Returns the number of methods
+    //Declared in _classFile
+    unsigned int methodsSize();
+    
+    //Returns the number of attributes
+    //Declared in _classFile
+    unsigned int attributesSize();
+    
+    
+    
+    
+    
+    
+#pragma mark Modifications
+    
+    /******************************************************
+     * Constructs a ConstantValue attribute of type Long
+     * and updates the Constant Pool.
+     * Returns the attribute_info
+     *****************************************************/
+    attribute_info addConstantValue_Long(CONSTANT_Long_info value);
+    
+    
+    /******************************************************
+     * Adds a field to the bytecode
+     * Performs some basic checks
+     * Adds the name_index and descriptor_index based in
+     * the fieldName and fieldDesc
+     * data into the constant pool
+     *****************************************************/
+    void addStaticField(u2 access_flags,
+                  const char *fieldName,
+                  const char *fieldDesc,
+                  u2 attributes_count,
+                  attribute_info  *attributes);
+    
+    /******************************************************
+     * Adds a native method to the bytecode
+     * Performs some basic checks
+     * Adds the name_index and descriptor_index based in
+     * the methodName and methodDesc
+     * data into the constant pool
+     *****************************************************/
+    
+    void addNativeMethod(u2 access_flags,
+                        const char *methodName,
+                        const char *methodDesc);
+    
+#pragma mark -
+
+    
+#pragma mark Static methods
+public:
+    
+    
+    /************************************************************************
+     *                                                                      *
+     *  Return the bytecode representations of Java types                   *
+     *                                                                      *
+     ************************************************************************/
+    
+    static const char *voidTypeToBytecode()     {return "V";}
+    
+    static const char *byteTypeToBytecode()     {return "B";}
+    
+    static const char *charTypeToBytecode()     {return "C";}
+    
+    static const char *doubleTypeToBytecode()   {return "D";}
+    
+    static const char *floatTypeToBytecode()    {return "F";}
+    
+    static const char *intTypeToBytecode()      {return "I";}
+    
+    static const char *longTypeToBytecode()     {return "J";}
+    
+    static const char *shortTypeToBytecode()    {return "S";}
+    
+    static const char *booleanTypeToBytecode()  {return "Z";}
+    
+    static const char *arrayTypeToBytecode()    {return "[";}
+    
+    //Returns the field type of a class
+    static const char *classRefTypeToBytecode(
+                      const char *reference)    {
+        
+        unsigned int ref_len = (unsigned int)strlen(reference);
+        char *ref = (char *)malloc(sizeof(char)*
+                                         (ref_len + 2));
+        
+        ref[0] = 'L';
+        memcpy(ref+1, reference, sizeof(char)*ref_len);
+        ref[ref_len+2] = ';';
+        return ref;
+    }
+    
+private:
+    //Returns the string of the access & properties flags
+    //If the method flag is set, then access_flags belongs to a method
+    //Else it belongs to a field
+    static const char *_accessModifiersToString(bool method,u2 access_flags)
+    {
+        char *flags[14];
+        bzero(flags, sizeof(char *)*14);
+        unsigned char index = 0; //Next free index in flags
+        unsigned long size = 0;
+        if(access_flags & ACC_PUBLIC){
+            flags[index] = (char *)malloc(7);
+            strcpy(flags[index], "PUBLIC");
+            size += 6;
+            index++;
+        }
+        if(access_flags & ACC_PRIVATE)
+        {
+            flags[index] = (char *)malloc(8);
+            strcpy(flags[index], "PRIVATE");
+            size += 8;
+            index++;
+        }
+        if(access_flags & ACC_PROTECTED)
+        {
+            flags[index] = (char *)malloc(10);
+            strcpy(flags[index], "PROTECTED");
+            size += 10;
+            index++;
+        }
+        if(access_flags & ACC_STATIC)
+        {
+            flags[index] = (char *)malloc(10);
+            strcpy(flags[index], "PROTECTED");
+            size += 10;
+            index++;
+        }
+        if(access_flags & ACC_FINAL)
+        {
+            flags[index] = (char *)malloc(6);
+            strcpy(flags[index], "FINAL");
+            size += 6;
+            index++;
+        }
+        if(access_flags & ACC_SYNCHRONIZED)
+        {
+            flags[index] = (char *)malloc(13);
+            strcpy(flags[index], "SYNCHRONIZED");
+            size += 13;
+            index++;
+        }
+        if((access_flags & ACC_VOLATILE) && !method){
+            flags[index] = (char *)malloc(9);
+            strcpy(flags[index], "VOLATILE");
+            size += 9;
+            index++;
+        }
+        if((access_flags & ACC_BRIDGE) && method){
+            flags[index] = (char *)malloc(7);
+            strcpy(flags[index], "BRIDGE");
+            size += 7;
+            index++;
+        }
+        if((access_flags & ACC_TRANSIENT) && !method){
+            flags[index] = (char *)malloc(10);
+            strcpy(flags[index], "TRANSIENT");
+            size += 10;
+            index++;
+        }
+        if((access_flags & ACC_VARARGS) && method){
+            flags[index] = (char *)malloc(8);
+            strcpy(flags[index], "VARARGS");
+            size += 8;
+            index++;
+        }
+        if(access_flags & ACC_NATIVE)
+        {
+            flags[index] = (char *)malloc(7);
+            strcpy(flags[index], "NATIVE");
+            size += 8;
+            index++;
+        }
+        if(access_flags & ACC_ENUM)
+        {
+            flags[index] = (char *)malloc(5);
+            strcpy(flags[index], "ENUM");
+            size += 5;
+            index++;
+        }
+        if(access_flags & ACC_ABSTRACT)
+        {
+            flags[index] = (char *)malloc(9);
+            strcpy(flags[index], "ABSTRACT");
+            size += 9;
+            index++;
+        }
+        if(access_flags & ACC_SYNTHETIC)
+        {
+            flags[index] = (char *)malloc(10);
+            strcpy(flags[index], "SYNTHETIC");
+            size += 10;
+            index++;
+        }
+        size++;
+        char *retChar = (char *)malloc(size);
+        unsigned short offset = 0;
+        for(unsigned char i=0; i != 14; i++){
+            if(!flags[i])
+                break;
+            strcpy(retChar+offset, flags[i]);
+            offset += strlen(flags[i]);
+            retChar[offset++] = ',';
+        }
+        retChar[offset-1] = 0;
+        return retChar;
+    }
+    
+#pragma mark -
+    
+#pragma mark Printing
+public:
+    void printMethodAt(unsigned short index);
+    
+#pragma mark -
+};
+
+
+
+
+#endif /* ClassFile_FirstParser_hpp */
---
+++ include/ClassFile_errs.hpp
@@ -0,0 +1,89 @@
+//
+//  ClassFile_errs.h
+//  
+//
+//  Created by Alfredo Cruz on 10/2/16.
+//
+//
+
+#ifndef ClassFile_errs_h
+#define ClassFile_errs_h
+
+/***
+ *
+ *  Defines all the exceptions thrown in case of error
+ *
+ */
+
+#pragma mark NoMagicNumber
+
+/**********************************************************
+ *           Thrown when no magic number match            *
+ **********************************************************/
+class NoMagicNumberMatchError{};
+
+#pragma mark -
+
+#pragma mark NoMemoryError
+/**********************************************************
+ *              Thrown when no memory error               *
+ **********************************************************/
+class NoMemoryError{};
+#pragma mark -
+
+#pragma mark TagNotRecognized
+/**********************************************************
+ *         Tag not recognized in the constant pool        *
+ **********************************************************/
+class ConstantPool_TagNotRecognized{};
+#pragma mark -
+/**********************************************************
+ *         Invalid access flag for field                  *
+ **********************************************************/
+#pragma mark FieldInvalidAccessFlag
+class FieldInvalidAccessFlag{};
+#pragma mark -
+/**********************************************************
+ *         Final and volatile flags are set               *
+ **********************************************************/
+#pragma mark FieldInvalidFinalVolatileFlags
+class FieldInvalidFinalVolatileFlags{};
+#pragma mark -
+/**********************************************************
+ *    No ConstantValue is defined for static attribute    *
+ **********************************************************/
+#pragma mark NoConstantValueAttributeForStaticField
+class NoConstantValueAttributeForStaticField{};
+#pragma mark -
+/**********************************************************
+ *    We intend to push an attribute to a structure that  *
+ *              shouldnt have any                         *
+ **********************************************************/
+#pragma mark FieldInfoDoesNotAdmitAttributes
+class FieldInfoDoesNotAdmitAttributes{};
+#pragma mark -
+/**********************************************************
+ *    New attribute index is greater than capacity        *
+ **********************************************************/
+#pragma mark FieldInfoNewAttributeIndexGraterThanCapacity
+class FieldInfoNewAttributeIndexGraterThanCapacity{};
+#pragma mark -
+/**********************************************************
+ *          Invalid index in the constant pool            *
+ **********************************************************/
+#pragma mark InvalidConstantPoolIndex
+class InvalidConstantPoolIndex{};
+#pragma mark -
+/**********************************************************
+ *      Unexpected type of data in the constant pool      *
+ **********************************************************/
+#pragma mark UnexpectedDataTypeAtConstantPoolIndex
+class UnexpectedDataTypeAtConstantPoolIndex{};
+#pragma mark -
+/**********************************************************
+ *          Invalid filename to dump bytecode             *
+ **********************************************************/
+#pragma mark InvalidFilenameToDumpBytecode
+class InvalidFilenameToDumpBytecode{};
+#pragma mark -
+#endif /* ClassFile_errs_h */
